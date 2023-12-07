30a06c52176311e77c5261c0734ef3d70e384634
i3tiger8 <i3tiger8@gmail.com>
2023-12-07 22:57:24 +0800

    update the Automate...code (photo click)

---
+++ codes/Exp_auto/photo_click.py
@@ -0,0 +1,18 @@
+import pyautogui
+import time
+import keyboard
+
+# 定义坐标（控制窗口大小为1/2屏幕，位于屏幕右侧位置）
+coords = {
+    "click": (2011, 197),
+}
+
+t1 = 5.5 # 间隔时间
+n = 1 # 点击数量
+print(f"------it starts------")
+while n <= 10:
+    pyautogui.click(coords["click"])
+    time.sleep(t1)
+    print(f"拍摄状态：已拍摄第{n}张照片")
+    n += 1
+print(f"------it's done------")
